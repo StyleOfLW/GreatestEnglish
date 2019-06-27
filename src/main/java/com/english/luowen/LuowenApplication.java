f68e674396345b6518160be8fbd811dd7699df4d
luowen <lw373984795@outlook.com>
2019-06-27 18:51:14 +0800

    One Greatest English Project

---
+++ src/main/java/com/english/luowen/LuowenApplication.java
@@ -0,0 +1,13 @@
+package com.english.luowen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuowenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuowenApplication.class, args);
+	}
+
+}
